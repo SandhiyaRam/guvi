0d1a4ef32e348551029b3acb7469ae7d4753d788
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-06-12 12:11:24 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,15 @@
+n=input()
+if n=='a':
+  print('vowel')
+elif n=='e':
+  print('vowel')
+elif n=='i':
+  print('vowel')
+elif n=='o':
+  print('vowel')
+elif n=='u':
+  print('vowel')
+elif n=='$':
+  print("invalid")
+else:
+  print("consonant")  
