abd754fbf201796f389cfee7aac54c527266a8fb
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-06-13 12:30:38 +0530

    Update amsrange.py

--- CodeKata/amsrange.py
+++ CodeKata/amsrange.py
@@ -1,10 +1,10 @@
-nos1,nos2 = map(int,input().split())
-for i in range(nos1+1,nos2):
-  sum = 0
-  temp = i
-  while temp > 0:
-    digit = temp % 10
-    sum += digit ** 3
-    temp //= 10
-  if i == sum:
+nosum1,nosum2 = map(int,input().split())
+for ii in range(nosum1+1,nosum2):
+  sumu = 0
+  tempu = ii
+  while tempu > 0:
+    digits = tempu % 10
+    sumu += digits ** 3
+    tempu //= 10
+  if ii == sumu:
     print(i,end=" ")
