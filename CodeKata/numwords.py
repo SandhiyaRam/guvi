3ce75810ea6262b06cc0a18098e9f88bdb605d57
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-06-14 12:36:59 +0530

    Create numwords.py

---
+++ CodeKata/numwords.py
@@ -0,0 +1,21 @@
+anty =int(input())
+if anty==1:
+  print("one")
+elif anty==2:
+  print("two")
+elif anty==3:
+  print("three")
+elif anty==4:
+  print("four")
+elif anty==5:
+  print("five")        
+elif anty==6:
+  print("six")
+elif anty==7:
+  print("seven")
+elif anty==8:
+  print("eight")
+elif anty==9:
+  print("nine")
+else:
+  print("ten")  
