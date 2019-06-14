167114c1f16c0e9e2b8134b39704a7d31db0e6b5
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-06-14 16:35:36 +0530

    Create occur.py

---
+++ CodeKata/occur.py
@@ -0,0 +1,11 @@
+ma,sa=map(int,input().split())
+lica=list(map(int,input().split()))
+cud=0
+if ma==len(lica):
+  for ai in range(ma):
+    if lica[ai]==sa:
+      cud=cud+1
+if cud>0:
+  print("Yes")
+else:
+  print("No")        
