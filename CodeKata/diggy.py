9c69caaa1c68af6281a2ea1ed4ea93519587c0c8
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-06-14 13:48:51 +0530

    Update and rename digsum.py to diggy.py

---
+++ CodeKata/diggy.py
@@ -0,0 +1,6 @@
+kaunty =input()
+ccc=0
+for cuuku in kaunty:
+  sai=int(c uku)
+  ccc=ccc+sai 
+print(ccc)  
--- CodeKata/digsum.py
+++
@@ -1,6 +0,0 @@
-aunty =input()
-cc=0
-for cuku in aunty:
-  sa=int(cuku)
-  cc=cc+sa 
-print(cc)  
