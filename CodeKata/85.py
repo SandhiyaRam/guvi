13572dd476fb5bf06052494055840c269fa57d89
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-06-17 19:39:19 +0530

    Update 85.py

--- CodeKata/85.py
+++ CodeKata/85.py
@@ -1,8 +1,9 @@
-Str=list(input())
-if len(Str)%2==0:
-    Str[int(len(Str)/2)] ='*'
-    Str[int(len(Str)/2)-1]='*'
+
+rar=list(input())
+if len(rar)%2==0:
+    rar[int(len(rar)/2)] ='*'
+    rar[int(len(rar)/2)-1]='*'
 else:
-    Str[int(len(Str)/2)] ='*'
-for i in range(0,len(Str)):
-    print(Str[i],end='')
+    rar[int(len(rar)/2)] ='*'
+for iui in range(0,len(rar)):
+    print(rar[iui],end='')
