04092e73e91eece38590959331337e78ba0868f4
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-06-19 20:36:15 +0530

    Update 108.py

--- CodeKata/108.py
+++ CodeKata/108.py
@@ -1,1 +1,11 @@
 
+ny=int(input())
+li=list(map(int,input().split()))
+a=li[0]
+if ny==len(li):
+    for i in range(1,len(li)):
+        if li[i]>a:
+              a=li[i]
+        else:
+            print(i-1)
+            break
