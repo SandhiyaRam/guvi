ec814ce5bca93e6a36c05efdcb613c7b03ca2bac
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-06-26 16:33:38 +0530

    Update *9.py

--- CodeKata/*9.py
+++ CodeKata/*9.py
@@ -1,1 +1,14 @@
 
+aa,bb=input().split(" ")
+aa=int(aa)
+bb=int(bb)
+co=0
+for i in range(aa,bb+1):
+    if(i>1):
+        for j in range(2,i):
+            if(i%j==0):
+                break
+                
+        else:
+            co=co+1
+print(co)
