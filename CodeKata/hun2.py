c804a6bb1e393e6596099c5af6fbef7c2b0d7f95
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-07-24 19:56:10 +0530

    Update hun2.py

--- CodeKata/hun2.py
+++ CodeKata/hun2.py
@@ -1,8 +1,12 @@
-count=int(input())
+cou=int(input())
 lissy=list(map(str,input().split()))
 em=[]
-for i in range(len(lissy)):
-    em.append(max(lissy))
-    lissy.remove(max(lissy))
+m=lissy[0]
+if lissy.count(m)==cou:
+    print(m)
+else:
+    for i in range(len(lissy)):
+        em.append(max(lissy))
+        lissy.remove(max(lissy))
 for i in em:
     print(i,end="")
