e22f0aef3a6e8a5df1585b6cc6c0f0c25b147c6a
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-07-28 16:22:05 +0530

    Update and rename hun1.py to huun1.py

--- CodeKata/huun1.py
+++ CodeKata/huun1.py
@@ -1,12 +1,17 @@
 num=input()
 li=list(map(str,input().split()))
 emp=[]
+temp=0
 for i in li:
     co=li.count(i)
     if co>1:
         if i not in emp:
             emp.append(i)
-
-emp.sort()
+            emp.sort()
 for i in emp:
     print(i,end=" ")
+for i in li:
+    if li.count(i)==1:
+        temp=temp+1
+if str(temp)==num:
+    print("unique")
