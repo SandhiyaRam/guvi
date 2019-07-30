4837d9c4bd9def7f8b8347465d9799461cd9efb2
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-07-30 21:10:34 +0530

    Update and rename pla32.py to player32.py

--- CodeKata/pla32.py
+++
@@ -1,6 +0,0 @@
-num1,num2=map(int,input().split())
-li=list(map(int,input().split()))
-if num2 in li:
-    print("Yes")
-else:
-    print("No")
---
+++ CodeKata/player32.py
@@ -0,0 +1,6 @@
+nnum1,nnum2=map(int,input().split())
+lni=list(map(int,input().split()))
+if nnum2 in lni:
+    print("Yes")
+else:
+    print("No")
