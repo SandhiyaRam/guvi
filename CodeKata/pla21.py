0e461aa353042cb2f224972ff75e4f89f755daac
SandhiyaRam <51657611+SandhiyaRam@users.noreply.github.com>
2019-07-30 20:44:34 +0530

    Update pla21.py

--- CodeKata/pla21.py
+++ CodeKata/pla21.py
@@ -1,7 +1,7 @@
 num1,num2=map(int,input().split())
 sum1,sum2=map(int,input().split())
 mum1,mum2=map(int,input().split())
-if(num1==sum1==mum1) or (num2==sum2==mum2):
+if(num1==num2) or (sum1==sum2) or (mum1==mum2):
     print("yes")
 else:
     print("no")
